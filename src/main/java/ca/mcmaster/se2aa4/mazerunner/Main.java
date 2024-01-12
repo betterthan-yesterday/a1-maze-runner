8ddc608774b47dbc44c35f39c637755b49c54994
williampol <103951515+williampol@users.noreply.github.com>
2024-01-12 13:14:29 -0500

    Add flag for input

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -11,9 +11,19 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
+        try {
+            if (!("-i".equals(args[0]) || "--input".equals(args[0]))) {
+                System.err.println("/!\\ An error has occured (no input) /!\\");
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            System.err.println("/!\\ An error has occured (no args)) /!\\");
+            System.exit(1);
+        }
+
         System.out.println("** Starting Maze Runner");
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
+            System.out.println("**** Reading the maze from file " + args[1]);
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             String line;
             while ((line = reader.readLine()) != null) {
@@ -26,8 +36,9 @@ public class Main {
                 }
                 System.out.print(System.lineSeparator());
             }
+            reader.close();
         } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            System.err.println("/!\\ An error has occured (file not found) /!\\");
         }
         System.out.println("**** Computing path");
         System.out.println("PATH NOT COMPUTED");
