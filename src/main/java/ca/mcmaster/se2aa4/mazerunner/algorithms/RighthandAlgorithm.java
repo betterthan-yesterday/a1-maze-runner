b2baaacf3d1faa624d303c81d22790f3e8331764
williampol <103951515+williampol@users.noreply.github.com>
2024-01-24 13:09:59 -0500

    Move checkPath code from PathAlgorithm.java to RightHandAlgorithm.

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/algorithms/RighthandAlgorithm.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/algorithms/RighthandAlgorithm.java
@@ -17,6 +17,39 @@ public class RighthandAlgorithm extends PathAlgorithm {
         this.mover = new Movement();
     }
 
+    public boolean checkRight(Direction dir) {
+        boolean right_is_path = false;
+        switch (dir) {
+            case NORTH -> right_is_path = maze_array[currentPos[0]][currentPos[1]+1] == 0;
+            case EAST -> right_is_path = maze_array[currentPos[0]+1][currentPos[1]] == 0;
+            case SOUTH -> right_is_path = maze_array[currentPos[0]][currentPos[1]-1] == 0;
+            case WEST -> right_is_path = maze_array[currentPos[0]-1][currentPos[1]] == 0;
+        }
+        return right_is_path;
+    }
+
+    public boolean checkFront(Direction dir) {
+        boolean front_is_path = false;
+        switch (dir) {
+            case NORTH -> front_is_path = maze_array[currentPos[0]-1][currentPos[1]] == 0;
+            case EAST -> front_is_path = maze_array[currentPos[0]][currentPos[1]+1] == 0;
+            case SOUTH -> front_is_path = maze_array[currentPos[0]+1][currentPos[1]] == 0;
+            case WEST -> front_is_path = maze_array[currentPos[0]][currentPos[1]-1] == 0;
+        }
+        return front_is_path;
+    }
+
+    public boolean checkLeft(Direction dir) {
+        boolean left_is_path = false;
+        switch (dir) {
+            case NORTH -> left_is_path = maze_array[currentPos[0]][currentPos[1]-1] == 0;
+            case EAST -> left_is_path = maze_array[currentPos[0]-1][currentPos[1]] == 0;
+            case SOUTH -> left_is_path = maze_array[currentPos[0]][currentPos[1]+1] == 0;
+            case WEST -> left_is_path = maze_array[currentPos[0]+1][currentPos[1]] == 0;
+        }
+        return left_is_path;
+    }
+
     @Override
     public MazePath solve() {
         String seq = "";
