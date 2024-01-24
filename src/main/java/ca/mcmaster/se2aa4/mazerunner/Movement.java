f5f4c349c82510f0012b2849dfa1d05997aea011
williampol <103951515+williampol@users.noreply.github.com>
2024-01-24 17:44:16 -0500

    Add path verification algorithm (close #24)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Movement.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Movement.java
@@ -10,7 +10,9 @@ public class Movement {
     }
 
     public int[] updatePos(Move move, Direction dir, int[] currentPos) {
-        int[] newPos = currentPos;
+        int[] newPos = new int[2];
+        newPos[0] = currentPos[0];
+        newPos[1] = currentPos[1];
         switch (dir) {
             case NORTH -> newPos[0] = (move == Move.FORWARD) ? currentPos[0] - 1 : currentPos[0] + 1;
             case EAST -> newPos[1] = (move == Move.FORWARD) ? currentPos[1] + 1 : currentPos[1] - 1;
